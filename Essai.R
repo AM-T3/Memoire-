e92f1b068cbe3f443d7ee55f9c973e16629a9e34
Judith-RR <judith.raineaurispal@gmail.com>
2025-03-13 15:19:56 +0100

    Nettoyage de donnée

--- Essai.R
+++ Essai.R
@@ -1,4 +1,6 @@
 getwd()
+library(tidyverse)
+library(ggplot2)
 Consomation<-read.csv("nominal_expenditures.csv")
 Population<-read.csv("pop.csv")
 Food_pattern<-read.csv("servings.csv") #ce que mange les gens en quatité dans le temps
@@ -10,3 +12,5 @@ Fastfood_2023<-read.csv("fastfood_2023.csv")
 Fastfood_2022<-read.csv("fastfood_2022.csv")
 Fastfood_2006<-read.csv("2016-Eating-and-Health-Module-Tables/Table4_2006.csv")
 
+Chronic_disease<-read.csv("Chronic_Disease_Indicators.csv")
+Obésité<-read.csv("2023-Obesity-by-demographic.csv")
